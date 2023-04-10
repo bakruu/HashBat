24f0223ab384f23bce89aec3597210af8e1ff2a5
Mert Ali Özçelik <bakruux@gmail.com>
2023-04-11 00:59:19 +0300

    Update hash_cracker.py

--- hash_cracker.py
+++ hash_cracker.py
@@ -19,3 +19,28 @@ list = word_list.splitlines()
 for word in lists:
     if hash_type == "MD5":
         hash_object = hashlib.md5(f"{word}".encode('utf-8'))
+        hashed = hash_object.hexdiges()
+        if hash == hashed:
+            print(f"\033[1;32m HASH FOUND: {word} \n")
+    elif hash_type =="SHA1":
+        hash_object = hashlib.sha1(f"{word}".encode('utf-8'))
+        hashed = hash_object.hexdiges()
+        if hash == hashed:
+            print(f"\033[1;32m HASH FOUND: {word} \n")
+    elif hash_type =="SHA224":
+        hash_object = hashlib.sha224(f"{word}".encode('utf-8'))
+        hashed = hash_object.hexdiges()
+        if hash == hashed:
+            print(f"\033[1;32m HASH FOUND: {word} \n")
+    elif hash_type =="SHA512":
+        hash_object = hashlib.sha512(f"{word}".encode('utf-8'))
+        hashed = hash_object.hexdiges()
+        if hash == hashed:
+            print(f"\033[1;32m HASH FOUND: {word} \n")
+    elif hash_type =="SHA384":
+        hash_object = hashlib.sha384(f"{word}".encode('utf-8'))
+        hashed = hash_object.hexdiges()
+        if hash == hashed:
+            print(f"\033[1;32m HASH FOUND: {word} \n")
+    else:
+        print("Please choose from given options.")
